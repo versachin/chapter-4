1f16ef559e47010160564c1e6ba667c8b17d9b80
versachin <31958628+versachin@users.noreply.github.com>
2017-10-19 14:58:47 -0400

    Chapter 4 Problem 9

---
+++ Chapter 4 Problem 9.py
@@ -0,0 +1,12 @@
+#Write a void function to draw a star,
+#where the length of each side is 100 units.
+#(Hint: You should turn the turtle by 144 degrees at each point.)
+import turtle
+alex=turtle.Turtle()
+def draw_star():
+    alex.right(216)
+    for i in range(4):
+        alex.forward(100)
+        alex.left(216)
+    alex.forward(100)
+draw_star()
