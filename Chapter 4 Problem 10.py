9be8542272af48aea39c4ccc500ed2b1a6a13c46
alannaarabian <31958747+alannaarabian@users.noreply.github.com>
2017-10-31 14:55:59 -0400

    chapter 4 problem 10 done

--- Chapter 4 Problem 10.py
+++ Chapter 4 Problem 10.py
@@ -2,10 +2,20 @@
 #turn right by 144, put the pen down, and draw the next star. You’ll get something like this:
 import turtle
 alex=turtle.Turtle()
+alex.color("hot pink")
+alex.pensize(3)
+wn=turtle.Screen()
+wn.bgcolor("light green")
 def draw_star():
     alex.right(216)
     for i in range(4):
         alex.forward(100)
         alex.left(216)
     alex.forward(100)
-draw_star()
+for i in range (5):
+    draw_star()
+    alex.penup()
+    alex.forward(350)
+    alex.right(144)
+    alex.pendown()
+                 
