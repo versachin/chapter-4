3b32892755285f8c1244eae13d90f7b12cbb8613
versachin <31958628+versachin@users.noreply.github.com>
2017-11-17 08:17:30 -0500

    Fixed 6

--- Chapter 4 Problem 6.py
+++ Chapter 4 Problem 6.py
@@ -4,9 +4,14 @@ s = turtle.Turtle()
 s.pensize(3)
 s.color("hotpink")
 wn.bgcolor("lightgreen")
-def draw_equitriangle(t, sz):
-    for i in range(3):
+def draw_poly(t,num,sz):
+    for i in range(num):
         t.forward(sz)
-        t.left(360/3)
-        
+        t.left(360/num) 
+def draw_equitriangle(t, sz):
+    draw_poly(s,3,100)
+    
+    
 draw_equitriangle(s, 100)
+    
+
