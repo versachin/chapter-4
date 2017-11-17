8085343412640c9e466cf2ef35a22a17fbc95b27
versachin <31958628+versachin@users.noreply.github.com>
2017-11-17 08:35:07 -0500

    Fixed

--- Chapter 4 Problem 8.py
+++ Chapter 4 Problem 8.py
@@ -1,23 +1,6 @@
-import turtle
-wn = turtle.Screen()
-wn.bgcolor("lightgreen")
-s = turtle.Turtle()
-s.color("hotpink")
-s.pensize(3)
-def draw_poly(turt,num,size):
-    for i in range(num):
-        turt.forward(size)
-        turt.left(360/num)
-
-
-def draw_equitriangle(t, sz):
-    for i in range(3):
-        t.forward(sz)
-        t.left(120)
-        
-draw_equitriangle(s, 100)
-        
-        
-
+def area_of_circle(r):
+    a = 3.14*r**2
+    return a
     
-
+    
+print(area_of_circle(7))
